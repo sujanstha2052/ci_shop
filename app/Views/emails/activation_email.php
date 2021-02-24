65953cf60649e8178b67234a58683ecc0c57f8e9
Sujan Stha <sujanstha2052@gmail.com>
2021-02-24 14:49:52 +0545

    - activation email template

---
+++ app/Views/emails/activation_email.php
@@ -0,0 +1,5 @@
+<h1>Account Activation</h1>
+<p>Please click on the link below to activate your account: </p>
+<p>
+	<a href="<?= site_url('/authentication/activate/$token') ?>">Activate account</a>
+</p>
