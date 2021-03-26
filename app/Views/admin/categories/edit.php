bc1e554b34558719c0938ba8edf7e59f370e8401
Sujan Stha <sujanstha2052@gmail.com>
2021-03-26 17:28:10 +0545

    - update

--- app/Views/admin/categories/edit.php
+++ app/Views/admin/categories/edit.php
@@ -4,26 +4,27 @@
 
 <?= $this->section('content') ?>
 <div class="ibox ">
-	<div class="ibox-content">
-		<h3>Update Category</h3>
-		<hr>
-		<div class="row">
-			<div class="col-md-12">
+    <div class="ibox-content">
+        <h3>Update Category</h3>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
 
-				<?= form_open("admin/categories/edit/" . $category->id) ?>
-				<div class="form-group">
-					<label for="cat_title">Category Title</label> 
-					<input type="text" id="cat_title" class="form-control" name="cat_title" value="<?= (isset($category->cat_title) ? $category->cat_title : old('cat_title')); ?>">
-				</div>
-				<div>
-					<button class="btn btn-sm btn-primary" type="submit">
-						<strong>Submit</strong>
-					</button>
-				</div>
-				<?= form_close() ?>
-			</div>
+                <?= form_open("admin/categories/edit/" . $category->id) ?>
+                <div class="form-group">
+                    <label for="cat_title">Category Title</label>
+                    <input type="text" id="cat_title" class="form-control" name="cat_title"
+                        value="<?= (isset($category->cat_title) ? $category->cat_title : old('cat_title')); ?>">
+                </div>
+                <div>
+                    <button class="btn btn-sm btn-primary" type="submit">
+                        <strong>Submit</strong>
+                    </button>
+                </div>
+                <?= form_close() ?>
+            </div>
 
-		</div>
-	</div>
+        </div>
+    </div>
 </div>
 <?= $this->endSection() ?>
